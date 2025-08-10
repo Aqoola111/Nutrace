b96aa97bfae0a9e0d8efbfea7f2150849f8a8f6c
david <datunebi2003@gmail.com>
2025-08-10 18:32:09 +0300

    add alert dialog provider and integrate with category cards for delete confirmation

--- src/app/admin/foods-management/categories/_components/category-cards.tsx
+++ src/app/admin/foods-management/categories/_components/category-cards.tsx
@@ -1,10 +1,11 @@
 'use client'
 import {useCategories} from "@/app/admin/foods-management/categories/_services/use-category-queries";
 import {useDeleteCategoryMutation} from "@/app/admin/foods-management/categories/_services/use-category-mutations";
-import {Card, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import {Card, CardHeader, CardTitle} from "@/components/ui/card";
 import {Button} from "@/components/ui/button";
 import {Edit, Loader, Trash2Icon} from "lucide-react";
 import {Skeleton} from "@/components/ui/skeleton";
+import {alert} from "@/lib/use-global-store";
 
 
 export const CategoryCards = () => {
@@ -26,24 +27,29 @@ export const CategoryCards = () => {
 			{categories.data?.map((category) => (
 				<Card key={category.id} className='w-full md:w-[300px]'>
 					<CardHeader>
-						<CardTitle>
+						<CardTitle className='flex justify-between items-center font-bold text-2xl'>
 							{category.name}
+							<div className='flex gap-2 items-center'>
+								<Button variant='default' size='icon'>
+									<Edit/>
+								</Button>
+								<Button size='icon' variant='secondary'
+										disabled={deleteCategoryMutation.isPending}
+										onClick={() => {
+											alert({
+												onConfirm: () => deleteCategoryMutation.mutate(category.id),
+												title: 'Delete Category',
+												description: `Are you sure you want to delete the category`,
+												confirmLabel: 'Delete',
+												cancelLabel: 'Cancel',
+											})
+										}}>
+									{deleteCategoryMutation.isPending ? <Loader className='animate-spin'/> :
+										<Trash2Icon/>}
+								</Button>
+							</div>
 						</CardTitle>
 					</CardHeader>
-					<CardFooter className='flex items-center justify-between'>
-						<Button variant='default'>
-							Edit
-							<Edit/>
-						</Button>
-						<Button variant='destructive'
-								disabled={deleteCategoryMutation.isPending}
-								onClick={() => {
-									deleteCategoryMutation.mutate(category.id)
-								}}>
-							{deleteCategoryMutation.isPending ? 'Deleting...' : 'Delete'}
-							{deleteCategoryMutation.isPending ? <Loader className='animate-spin'/> : <Trash2Icon/>}
-						</Button>
-					</CardFooter>
 				</Card>
 			))}
 		</div>
--- src/components/providers/providers.tsx
+++ src/components/providers/providers.tsx
@@ -1,6 +1,7 @@
 'use client'
 import {ThemeProvider} from "@/components/providers/theme-provider";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {AlertDialogProvider} from "@/components/providers/alert-dialog-provider";
 
 interface ProvidersProps {
 	children?: React.ReactNode;
@@ -12,6 +13,7 @@ export const Providers = ({children}: ProvidersProps) => {
 	return (
 		<ThemeProvider>
 			<QueryClientProvider client={queryClient}>
+				<AlertDialogProvider/>
 				{children}
 			</QueryClientProvider>
 		</ThemeProvider>
